d11838fa906b543548ac9f7cd006a563b3d30311
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-11-21 18:52:23 +0100

    Prof Login

--- app/containers/Login/LoginPage.js
+++ app/containers/Login/LoginPage.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-import { connect } from 'react-router';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import { Checkbox, Alert, Icon, Form, Input, Button } from 'antd';
 import styles from './LoginPage.less';
 
+import { profLogin as _profLogin } from '../../actions/session'
+
 const FormItem = Form.Item;
+
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(field => fieldsError[field]);
+}
 
 class LoginPage extends Component {
   state = {
-    type: 'account',
     autoLogin: true,
   };
 
+  componentDidMount() {
+    // To disabled submit button at the beginning.
+    this.props.form.validateFields()
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
+    const {
+      profLogin,
+      history,
+      form: { validateFields }
+    } = this.props
+    
+    const { autoLogin } = this.state
+
+    validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        profLogin(values.email, values.password)
+          .then((prof) => {
+            history.push('/')
+            return prof
+          })
+          .catch(() => null)
       }
     });
   }
@@ -33,24 +55,34 @@ class LoginPage extends Component {
 
   render() {
     const {
-      login,
-      submitting,
-      form: { getFieldDecorator }
+      form: { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched },
+      submitError
     } = this.props;
 
-    const { type, autoLogin } = this.state;
-    
+    const emailError = isFieldTouched('email') && getFieldError('email');
+    const passwordError = isFieldTouched('password') && getFieldError('password');
+
     return (
       <div className={styles.main}>
+        {submitError && this.renderMessage(submitError)}
         <Form onSubmit={this.handleSubmit} className="login-form">
-          <FormItem>
-            {getFieldDecorator('userName', {
-              rules: [{ required: true, message: 'Please input your username!' }],
+          <FormItem
+            validateStatus={emailError ? 'error' : ''}
+            help={emailError || ''}
+          >
+            {getFieldDecorator('email', {
+              rules: [
+                { type: 'email', message: 'The email is not valid!' },
+                { required: true, message: 'Please input your email!' },
+              ],
             })(
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="email" />
             )}
           </FormItem>
-          <FormItem>
+          <FormItem
+            validateStatus={passwordError ? 'error' : ''}
+            help={passwordError || ''}
+          >
             {getFieldDecorator('password', {
               rules: [{ required: true, message: 'Please input your Password!' }],
             })(
@@ -66,7 +98,12 @@ class LoginPage extends Component {
               <Checkbox>Remember me</Checkbox>
             )}
             <a style={{ float: 'right' }} href="">Forgot password</a>
-            <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+            <Button
+              type="primary"
+              htmlType="submit"
+              style={{ width: '100%' }}
+              disabled={hasErrors(getFieldsError())}
+            >
               Log in
             </Button>
           </FormItem>
@@ -78,4 +115,12 @@ class LoginPage extends Component {
 
 const WrappedLoginPage = Form.create()(LoginPage);
 
-export default WrappedLoginPage;
+const mapStateToProps = state => ({
+  submitError: state.session.loginError
+});
+
+
+export default connect(
+  mapStateToProps,
+  { profLogin: _profLogin }
+)(WrappedLoginPage);
