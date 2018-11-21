ccca7cfb865105aaa666444be1ef3a3f6243a576
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-11-21 18:49:26 +0100

    code clean

--- app/database.js
+++ app/database.js
@@ -1,6 +1,27 @@
 import PouchDB from 'pouchdb';
 PouchDB.plugin(require('pouchdb-find'));
 
+// class PuchDB {
+
+//   static db;
+
+//   static instance;
+
+//   constructor(){
+  //     if(PuchDB.instance){
+    //       return PuchDB.instance;
+    //     }
+    
+    //     PuchDB.instance = this;
+    //     console.log('****** !!!!connecting to db')
+    //     PuchDB.db = new PouchDB('./pouchdb');
+    //   }
+    
+    // }
+    
+    
+// export default PuchDB.db;
+    // const db = PuchDB.db
 const db = new PouchDB('./pouchdb' )
 
 db.createIndex({
@@ -17,3 +38,4 @@ export const find = (request) => (db.find(request));
 export const getById = (id = '') => (db.get(id));
 
 export const remove = (doc, options) => (db.remove(doc, options));
+
