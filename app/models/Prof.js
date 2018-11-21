4a64db603f6514e3fdfa49d568c548a0cfd54bbc
Mehdi ElHAIJ <mehdi.elhaij@hotmail.com>
2018-11-21 18:46:52 +0100

    new ORM: Prof

--- app/models/Prof.js
+++ app/models/Prof.js
@@ -1,5 +1,32 @@
 import {store, find as _find, getById as _getById} from '../database'
+import Model, { Joi } from '../utils/Model'
 
+const Prof = Model("prof", Joi.object().keys({
+  firstName: Joi.string().min(3).max(30).required(),
+  lastName: Joi.string().min(3).max(30).required(),
+  phone: Joi.string(),
+
+  email: Joi.string().email({ minDomainAtoms: 2 }),
+  password: Joi.string().min(6).required(),
+
+  role: Joi.string().equal(['prof', 'admin']).default('prof')
+}));
+
+Prof.login = async (email, password) => {
+  const result = await Prof.find({ email })
+
+  if(result.length === 0)
+    throw new Error('email not found')
+  
+  const prof = result[0]
+
+  if(prof.password === password)
+    return new Prof(prof)
+
+  throw new Error('password not correct')
+}
+
+export default Prof;
 
 export const save = (doc) => (
   store({
